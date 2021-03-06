bcccf8b94f3edc5d6cc7cda2f17e6de18ce51d6f
Emile Broeders <emile@emile.tech>
2021-03-06 20:19:54 +0100

    wip: Setup of Counter infra code

---
+++ src/infra/Counter/index.ts
@@ -0,0 +1,1 @@
+// todo: Make this
--- src/infra/index.ts
+++ src/infra/index.ts
@@ -1,20 +1,13 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as resources from "@pulumi/azure-native/resources";
-import * as storage from "@pulumi/azure-native/storage";
+import * as azure from "@pulumi/azure";
 
-// Create an Azure Resource Group
-const resourceGroup = new resources.ResourceGroup("resourceGroup");
+const prefix = `${pulumi.getProject()}-${pulumi.getStack()}`.toLowerCase();
 
-// Create an Azure resource (Storage Account)
-const storageAccount = new storage.StorageAccount("sa", {
-    resourceGroupName: resourceGroup.name,
-    sku: {
-        name: storage.SkuName.Standard_LRS,
-    },
-    kind: storage.Kind.StorageV2,
+const resourceGroup = new azure.core.ResourceGroup(`${prefix}-rg`, {
+  tags: {
+    project: pulumi.getProject(),
+    environment: pulumi.getStack(),
+  },
 });
 
-// Export the primary key of the Storage Account
-const storageAccountKeys = pulumi.all([resourceGroup.name, storageAccount.name]).apply(([resourceGroupName, accountName]) =>
-    storage.listStorageAccountKeys({ resourceGroupName, accountName }));
-export const primaryStorageKey = storageAccountKeys.keys[0].value;
+
